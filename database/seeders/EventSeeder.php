268b13b70334afa7731e933bd69f213d39bdca3e
unfor <unforu0@gmail.com>
2022-11-21 08:49:43 +0300

    add seeder and factories

---
+++ database/seeders/EventSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
